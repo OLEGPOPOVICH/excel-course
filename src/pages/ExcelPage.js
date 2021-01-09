dd64aff35e713647158f5464b3c2534028d65ae3
Попович Олег Игоревич <OIPopovich@sberbank.ru>
2021-01-10 02:27:41 +0300

    Implement DIP

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,29 +1,29 @@
-import {Page} from '@core/Page';
+import {Page} from '@core/page/Page';
 import {Excel} from '@/components/excel/Excel';
 import {Header} from '@/components/header/Header';
 import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import {createStore} from '@core/store/createStore';
-import {storage} from '@core/utils';
 import {rootReducer} from '@/redux/rootReducer';
-import {deBounce} from '@core/utils';
 import {normalizeInitialState} from '@/redux/initialState';
+import {StateProcessor} from '../core/page/StateProcessor';
+import {LocalStorageClient} from '../shared/LocalStorageClient';
 
-function storageName(param) {
-  return `excel:${param}`;
-}
 export class ExcelPAge extends Page {
-  getRoot() {
-    const params = this.params ? this.params : Date.now().toString();
-    const state = storage(storageName(params));
+  constructor(param) {
+    super(param)
+    this.storeSub = null;
+    this.processor = new StateProcessor(
+        new LocalStorageClient(this.params)
+    );
+  }
+
+  async getRoot() {
+    const state = await this.processor.get()
     const store = createStore(rootReducer, normalizeInitialState(state));
 
-    const stateListener = deBounce((state) => {
-      storage(storageName(params), state);
-    }, 300);
-
-    store.subscribe(stateListener);
+    this.storeSub = store.subscribe(this.processor.listen);
 
     this.excel = new Excel({
 		  components: [
@@ -35,14 +35,15 @@ export class ExcelPAge extends Page {
 		  store
     });
 
-    return this.excel.getRoot()
+    return this.excel.getRoot();
   }
 
   afterRender() {
-    this.excel.init()
+    this.excel.init();
   }
 
   destroy() {
-	  this.excel.destroy()
+	  this.excel.destroy();
+	  this.storeSub.unsubscribe();
   }
 }
